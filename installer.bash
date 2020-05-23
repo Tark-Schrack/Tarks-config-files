4bbfb7fc3ac58845fd15baa55d5245fe468ed676
Tark-Schrack <64704691+Tark-Schrack@users.noreply.github.com>
2020-05-23 23:13:32 +0200

    Update installer.bash

--- installer.bash
+++ installer.bash
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
